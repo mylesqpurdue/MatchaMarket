623252e81bfb3fa101390e60ac07b119d081f8f2
mylesqpurdue <195245736+mylesqpurdue@users.noreply.github.com>
2025-04-20 02:52:38 -0500

    academic integrity

--- app.py
+++ app.py
@@ -1,3 +1,35 @@
+
+"""
+Course Number: ENGR 13300
+Semester: Spring 2025
+
+Description:
+    MatchaMarket, an Intutitive Stock Dashboard
+
+Assignment Information:
+    Assignment:     18.4.1
+    Author:         Myles Querimit, mquerimi@purdue.edu
+    Date:           02/25/2025
+
+Contributors:
+    Name, login@purdue [repeat for each]
+
+    My contributor(s) helped me:
+    [ ] understand the assignment expectations without
+        telling me how they will approach it.
+    [ ] understand different ways to think about a solution
+        without helping me plan my solution.
+    [ ] think through the meaning of a specific error or
+        bug present in my code without looking at my code.
+    Note that if you helped somebody else with their code, you
+    have to list that person as a contributor here as well.
+
+Academic Integrity Statement:
+    I have not used source code obtained from any unauthorized
+    source, either modified or unmodified; nor have I provided
+    another student access to my code.  The project I am
+    submitting is my own original work.
+"""
 import os
 import sys
 import json
